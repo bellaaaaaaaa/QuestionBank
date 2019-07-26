7c65edf044eca2d6b6ddd07bbb2f614e4438bc59
patricia-wong <patricia.wong@thetechyhub.com>
2019-07-26 13:04:56 +0800

    Admin navbar

--- resources/views/layouts/partials/admin/nav.blade.php
+++ resources/views/layouts/partials/admin/nav.blade.php
@@ -1,25 +1,17 @@
 
-<nav class="navbar navbar-expand-lg navbar-absolute navbar-transparent mt-0" id="nav-item-color">
+<nav class="navbar navbar-expand-lg navbar-absolute navbar-light mt-0" id="nav-item-color">
  <div class="container-fluid">
    <div class="navbar-wrapper">
-     <div class="navbar-toggle">
-       <button type="button" class="navbar-toggler">
-         <span class="navbar-toggler-bar bar1"></span>
-         <span class="navbar-toggler-bar bar2"></span>
-         <span class="navbar-toggler-bar bar3"></span>
-       </button>
-     </div>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
    </div>
-   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
-     <span class="navbar-toggler-bar navbar-kebab"></span>
-     <span class="navbar-toggler-bar navbar-kebab"></span>
-     <span class="navbar-toggler-bar navbar-kebab"></span>
-   </button>
+
    <div class="collapse navbar-collapse justify-content-end" id="navigation">
      <ul class="navbar-nav">
-       <li class="nav-item">
+       <li class="nav-item mobile-log">
          <a class="nav-link btn-magnify" href="{{ route('admin.dashboard') }}">
-           <i class="nc-icon nc-layout-11"></i>
+           <i class="fa fa-bars"></i>
            <p>
              <span class="d-lg-none d-md-block">Dashboard</span>
            </p>
@@ -27,10 +19,7 @@
        </li>
        <li class="nav-item btn-rotate dropdown">
          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-           <i class="fa fa-cog"></i>
-           <p>
-             <span class="d-lg-none d-md-block">Some Actions</span>
-           </p>
+           <i class="fa fa-cog"></i><span>Settings</span>
          </a>
          <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
            <a class="dropdown-item" href="{{ route('admin.account.show') }}">Edit Profile</a>
